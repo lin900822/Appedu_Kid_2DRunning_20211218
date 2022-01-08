ee3e7047ce9a829f5f638539b252bd2e2861d302
Lin <wilson24386433@gmail.com>
2022-01-08 21:04:11 +0800

    腳色動畫，物理系統

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public int ccCount = 1000;
+    public float weight = 3.5f;
+    public string brand = "Toyota";
+    public bool hasWindow = false;
+
+    public Color color;
+    public Color yellow = Color.yellow;
+    public Color magenta = Color.magenta;
+}
